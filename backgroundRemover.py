cbf1ba4ec052bc3afb34afcaa0f79313a8463137
MarosPL <100544874+MarosPL@users.noreply.github.com>
2022-10-12 02:45:15 +0200

    added message box

--- backgroundRemover.py
+++ backgroundRemover.py
@@ -1,30 +1,40 @@
 # use PIP install rembg before using
 # use PIP install tkinter before using
 
+import tkinter
+import tkinter.messagebox
 from tkinter import *
 from tkinter import filedialog
 from rembg import remove
 from PIL import Image
 import os.path
+from sys import exit
 
 
 def Main():
     try:
         input_path = GetInputFile()
+        if input_path == '':
+            exit()
         output_path = input_path[:str.rindex(input_path, '.')] + '_output.png'
         RemoveBackground(input_path, output_path)
-        if not os.path.isfile(output_path):
-            print('Operation unsuccessful!')
-        else:
-            print('Operation successful!')
+        if os.path.isfile(output_path):
+            print('************* Operation successful!')
+            print('************* Output directory:')
+            print('************* ' +
+                  output_path[:str.rindex(output_path, '/')])
+            print('************* File name:')
+            print('************* ' +
+                  output_path[str.rindex(output_path, '/')+1:])
     except FileNotFoundError:
-        print('File ' + input_path + ' not found!')
+        print('Input file ' + input_path + ' not found!')
     except Exception as e:
         if e.__cause__ == 'RGBA':
             print(
                 "Please change extension of your output file. Acceptable format is png.")
         else:
             print(e.args)
+            print(e)
 
 
 def RemoveBackground(input_path, output_path):
@@ -34,10 +44,19 @@ def RemoveBackground(input_path, output_path):
 
 
 def GetInputFile():
-    root = Tk()
-    root.withdraw()
-    root.filename = filedialog.askopenfilename(
-        initialdir="/", title="Select file", filetypes=(("jpeg files", "*.jpg"), ("png files", "*.png")))
+    response = False
+    while response == False:
+        root = Tk()
+        root.withdraw()
+        root.filename = filedialog.askopenfilename(
+            initialdir="/", title="Remove background from image : Select file to edit", filetypes=[("PNG", "*.png"), ("JPG", "*.jpg"), ("JPEG", "*.jpeg"), ("ICON", "*.ico"), ("ALL", '*.*')])
+        if not root.filename:
+            response = tkinter.messagebox.askyesno(
+                "No file selected", "Do you want to quit?")
+            if response:
+                exit()
+        else:
+            response = True
     return root.filename
 
 
